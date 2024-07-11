2d8e6794826ed338aa6832eca0fc2d807bf3b535
remkv <154243179+remkv@users.noreply.github.com>
2024-07-11 12:16:48 +0530

    mongo db url as env variable

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // server.js
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
@@ -10,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 const authRoutes = require('./routes/auth');
 const taskRoutes = require('./routes/tasks');
 
-mongoose.connect('mongodb://localhost:27017/taskmanager', {
+mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
